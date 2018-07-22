9735399256cef34ec0451fd239affbe1e162d40f
Drezir <adamostrozlik1@gmail.com>
2018-07-22 16:07:50 +0200

    Soap

--- javaws.simple/src/main/java/teams/TeamsPublisher.java
+++
@@ -1,9 +0,0 @@
-package teams;
-
-import javax.xml.ws.Endpoint;
-
-public class TeamsPublisher {
-    public static void main(String[] args) {
-        Endpoint.publish("http://localhost:8080/teams", new TeamsImpl());
-    }
-}
--- javaws.simple/src/main/java/timeserver/TimeServerRunner.java
+++
@@ -1,9 +0,0 @@
-package timeserver;
-
-import javax.xml.ws.Endpoint;
-
-public class TimeServerRunner {
-    public static void main(String[] args) {
-        Endpoint.publish("http://127.0.0.1:8080/ts", new TimeServerImpl());
-    }
-}
